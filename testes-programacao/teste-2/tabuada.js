3452bd7d6a08f6cadd1f7daad2a74f688fc65cb3
Rafael <rafaeldebner08@gmail.com>
2022-08-25 20:50:00 -0300

    testes

---
+++ testes-programacao/teste-2/tabuada.js
@@ -0,0 +1,9 @@
+let multiplicado = 10
+let multiplicando = 1
+while (multiplicando<=100){
+    let resultado = multiplicado * multiplicando
+    let texto = `${multiplicado} x ${multiplicando}`
+    console.log(texto)
+    console.log(resultado)
+    multiplicando = multiplicando + 1
+}
