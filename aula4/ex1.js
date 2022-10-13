43f5527bec1d41b1484d637e339cad986b197091
Rafael <rafaeldebner08@gmail.com>
2022-10-13 20:19:47 -0300

    sla

---
+++ aula4/ex1.js
@@ -0,0 +1,10 @@
+const nome = "Pedro" 
+const anoDeNascimento = 2010
+const anoAtual = 2022
+const idade = anoAtual - anoDeNascimento
+
+if (idade >= 18){
+    console.log("Pode jogar!")
+} else {
+    console.log("acesso negado")
+}
